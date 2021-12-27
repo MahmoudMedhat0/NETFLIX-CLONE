9c03207cbce41581b50cc8ffc315ab77158ba766
mahmoud medhat <mahmoud.medhat39@yahoo.com>
2021-12-27 20:11:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home";
+import './app.scss'
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register";
+
+const App = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default App;
